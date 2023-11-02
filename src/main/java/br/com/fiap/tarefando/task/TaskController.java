6ce5e39b9e373dce053f8cc58bd8aed3ddae835b
Guilherme Maelby <107452235+GuiMaelby@users.noreply.github.com>
2023-11-02 02:14:32 -0300

    Add files via upload

---
+++ src/main/java/br/com/fiap/tarefando/task/TaskController.java
@@ -0,0 +1,84 @@
+package br.com.fiap.tarefando.task;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.fiap.tarefando.user.User;
+import jakarta.validation.Valid;
+
+@Controller
+@RequestMapping("/task")
+public class TaskController {
+
+    @Autowired
+    TaskService service;
+
+    @Autowired
+    MessageSource message;
+
+    @GetMapping
+    public String index(Model model, @AuthenticationPrincipal OAuth2User user){
+        model.addAttribute("avatar_url", user.getAttribute("avatar_url"));
+        model.addAttribute("username", user.getAttribute("name"));
+        model.addAttribute("tasks", service.findAll());
+        return "task/index";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String delete(@PathVariable Long id, RedirectAttributes redirect){
+        service.delete(id);
+        redirect.addFlashAttribute("success", message.getMessage("task.delete.success", null, LocaleContextHolder.getLocale()));
+        return "redirect:/task";
+    }
+
+    @GetMapping("new")
+    public String form(Task task, Model model, @AuthenticationPrincipal OAuth2User user){
+         model.addAttribute("avatar_url", user.getAttribute("avatar_url"));
+        model.addAttribute("username", user.getAttribute("name"));
+        return "task/form";
+    }
+
+    @PostMapping
+    public String create(@Valid Task task, BindingResult result , RedirectAttributes redirect){
+        if (result.hasErrors()) return "task/form";
+        service.create(task);
+        redirect.addFlashAttribute("success", "Tarefa cadastrada com sucesso");
+        return "redirect:/task";
+    }
+    
+    @GetMapping("dec/{id}")
+    public String decrement(@PathVariable Long id){
+        service.decrement(id);
+        return "redirect:/task";
+    }
+
+    @GetMapping("inc/{id}")
+    public String increment(@PathVariable Long id){
+        service.increment(id);
+        return "redirect:/task";
+    }
+
+    @GetMapping("catch/{id}")
+    public String catchTask(@PathVariable Long id, @AuthenticationPrincipal OAuth2User user){
+        service.catchTask(id, User.convert(user));
+        return "redirect:/task";
+    }
+
+    @GetMapping("drop/{id}")
+    public String dropTask(@PathVariable Long id, @AuthenticationPrincipal OAuth2User user){
+        service.dropTask(id, User.convert(user));
+        return "redirect:/task";
+    }
+    
+}
---
+++ src/test/java/br/com/fiap/tarefando/TarefandoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.tarefando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarefandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
